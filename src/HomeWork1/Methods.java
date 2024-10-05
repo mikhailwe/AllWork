71689280d0da2ca403fd00eb7bb4ff24c3e3caea
mikhailwe <mishagorchakov793@gmail.com>
2024-10-05 19:15:43 +0300

    dz za 29.09

--- src/HomeWork1/Methods.java
+++ src/HomeWork1/Methods.java
@@ -4,28 +4,36 @@ import java.util.Arrays;
 
 public class Methods {
     public static void main(String[] args) {
-//        0. Создайте новый класс Methods и все методы делайте в нём (методы должны быть статическими),
-//        а вызывать их надо из метода main, как обычно (как мы делали с Calculator на занятии).
-//        Т.е. нужно каждый метод вызвать с разными параметрами, чтобы проверить их работу.
 
-//                Обращайте внимание на хорошие названия переменных, методов и параметров методов, а также на форматирование кода
-//                (применяйте автоформат)
-//        1. Создайте метод square, который принимает одно целое число и возвращает его квадрат, затем вызовите его с разными значениями
-//        и выведите результат
-//        2. Создайте метод printFullName, который принимает два параметра типа String — имя и фамилию — и выводит полное имя в формате:
-//        "Full name: Имя Фамилия"
-//        3. Создайте метод cornerValuesArray, который принимает массив строк, а возвращает массив строк из двух элементов –
-//        самая короткая и самая длинная строка в исходном массиве
     }
 
+    /** Javadoc
+     * Метод для вычисления квадрата из принимаемого числа из другого класса.
+     * @param number тип переменной
+     * @return возвращает корень числа int
+     */
     static int square(int number) {
-
         return number * number;
     }
+
+    /** Javadoc
+     *
+     * @param name  тип переменной "Имя"
+     * @param surname тип переменной "Фамилия"
+     * @return
+     */
+
     static String printFullName(String name, String surname) {
         return name + " " + surname;
     }
-    static String cornerValuesArray(String[] word){
+
+    /** Javadoc
+     *
+     * @param word это массив строк (varargs).
+     * @return это строка, представляющая массив с коротким и длинным словом.
+     */
+
+    static String cornerValuesArray(String... word) {
         String longWord = word[0];
         String shortWord = word[0];
         for (String str : word) {
@@ -39,5 +47,46 @@ public class Methods {
         String[] shWordAndLoWord = {shortWord, longWord};
         return Arrays.toString(shWordAndLoWord);
     }
-}
 
+    /** Javadoc
+     *
+     * @param numbers varargs и тип переменной Numbers массив целых чисел, из которого будут рассчитаны минимальное и максимальное значения.
+     * @return возвращает инт значение. строковое представление массива, содержащего минимальное и максимальное значения.
+     */
+
+    static String cornerValuesArray(int... numbers) {
+    int min = numbers[0];
+    int max = numbers[0];
+    for (int i=0; i<numbers.length; i++) {
+        if (numbers[i] < min) {
+            min = numbers[i];
+        }
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    int[] result = new int[]{min,max};
+    return Arrays.toString(result);
+    }
+
+    /** Javadoc
+     * Находит минимальный и максимальный символы в заданном массиве символов.
+     * @param word varargs и тип переменной
+     * @return Строковое представление массива, содержащего минимальный и максимальный символы.
+     */
+
+    static String cornerValuesArray(char... word) {
+        char min = word[0];
+        char max = word[0];
+        for (int i=0; i<word.length; i++) {
+            if (word[i] < min) {
+                min = word[i];
+            }
+            if (word[i] > max) {
+                max = word[i];
+            }
+        }
+        char[] result1 = new char[]{min,max};
+        return Arrays.toString(result1);
+    }
+}
--- src/HomeWork1/mian.java
+++ src/HomeWork1/mian.java
@@ -2,12 +2,14 @@ package HomeWork1;
 
 public class mian {
     public static void main(String[] args) {
-
         System.out.println(Methods.square(6));
 
-        System.out.println(Methods.printFullName("Михаил" ,"Горчаков"));
+        System.out.println(Methods.printFullName("Михаил", "Горчаков"));
 
-        System.out.println(Methods.cornerValuesArray(new String[]{"I like work for IT", "Hello world" , "Very short", "asdq"}));
+        System.out.println(Methods.cornerValuesArray(new String[]{"I like work for IT", "Hello world", "Very short", "asdq"}));
+        System.out.println(Methods.cornerValuesArray(new int[]{55, 11, 5, 123, 87}));
+        System.out.println(Methods.cornerValuesArray(new char[]{'a', 'v', 's', 'w'}));
+
     }
 }
 
