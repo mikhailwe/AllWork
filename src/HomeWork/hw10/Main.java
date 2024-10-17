0cee8fef56eae0b04d2ffcb01179ca2d714014f4
mikhailwe <mishagorchakov793@gmail.com>
2024-10-17 18:11:55 +0300

    dz za 13.10

--- src/HomeWork/hw10/Main.java
+++ src/HomeWork/hw10/Main.java
@@ -3,24 +3,39 @@ package HomeWork.hw10;
 public class Main {
     public static void main(String[] args) {
 
-    Author author1= new Author("Ivan","Ivanovich");
-    Author author2= new Author("Petr","Petrovich");
+        Author author1 = new Author("Ivan", "Ivanovich");
+        Author author2 = new Author("Petr", "Petrovich");
 
-    Book book1= new Book("Split",1995,author1);
-    Book book2= new Book("HarryPotter",2001,author2);
-        System.out.println(book1.getBookInfo());
-        System.out.println(book2.getBookInfo());
+        Book book1 = new Book("Split", author1, 1995);
+        Book book2 = new Book("HarryPotter", author2, 2001);
 
-        author1.setName("Andrey");
-        author1.setSurname("Gubarev");
-        book1.setDatePublication(2020);
-        book1.setTitle("memoryForAndrю");
-        System.out.println(book1.getBookInfo());
+        System.out.println(author1);
+        System.out.println(author2);
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(" ");
+
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+        System.out.println(" ");
+
+
+        book1.setDatePublication(1985);
+        book1.setTitle("Титаник");
+        System.out.println(book1.toString());
+        System.out.println(" ");
 
         author2.setName("Mikle");
         author2.setSurname("Gorchakov");
-        System.out.println(author2.getName());
+        System.out.println(book2.toString());
+
+        System.out.println("Название кники " + book1.getTitle());
+        System.out.println("Имя автора " + book1.getAuthor());
+        System.out.println("Дата публикации " + book1.getDatePublication());
+        System.out.println("Название кники " + book2.getTitle());
+        System.out.println("Имя автора " + book2.getAuthor());
+        System.out.println("Дата публикации " + book2.getDatePublication());
 
 
-}
+    }
 }
